510e038bd118cd7c0bb7781233e3c602e554cc8f
Javier Romero <javier_aromero@Hotmail.com>
2020-12-29 21:51:04 -0500

    Heroku deployment

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const express = require('express');
+// const path = require('path');
 
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/mighty-beyond-95599'));
+
+// app.get('/*', function(req,res) {
+    
+// res.sendFile(path.join(__dirname+'/dist/mighty-beyond-95599/index.html'));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/mighty-beyond-95599'));
+app.use(express.static('./src'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/mighty-beyond-95599/index.html'));
-});
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'src'}
+  );
+  });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+  app.listen(process.env.PORT || 8080);
