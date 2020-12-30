b2f1dfed48db9fc51740042eeec3bf51e176aa74
Javier Romero <javier_aromero@Hotmail.com>
2020-12-29 22:38:10 -0500

    Heroku deployment

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.static('./src'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'src'}
+    res.sendFile('', {root: 'src'}
   );
   });
 
